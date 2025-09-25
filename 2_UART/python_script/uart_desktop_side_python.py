f66a1083c0efefd6fe83834832cd6ff58e47fc2b
r99941001 <38895939+r99941001@users.noreply.github.com>
2025-09-25 11:10:05 -0400

    add python file to receive msg from FPGA

---
+++ 2_UART/python_script/uart_desktop_side_python.py
@@ -0,0 +1,26 @@
+import serial
+
+def main():
+    # Change COM3 to the COM port your UART adapter uses
+    # Baudrate must match your FPGA/device UART setting
+    print("start receive data from FPGM test")
+    ser = serial.Serial(port="COM5", baudrate=115200, timeout=1)
+
+    print(f"Listening on {ser.port} at {ser.baudrate} baud...")
+
+    try:
+        while True:
+            if ser.in_waiting > 0:  # Check if data is available
+                print("have data")
+                data_temp = ser.readline()
+                print(data_temp)
+                data = ser.readline().decode(errors="ignore").strip()
+                if data:
+                    print(f"Received: {data}")
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        ser.close()
+
+if __name__ == "__main__":
+    main()
